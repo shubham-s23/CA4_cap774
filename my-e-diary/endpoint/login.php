e2b5a4892e7357390c9fc870c32df892b3d7f5b5
Shubham Shamrma <63958987+shubham-s23@users.noreply.github.com>
2024-11-22 11:29:35 +0530

    Add files via upload

---
+++ my-e-diary/endpoint/login.php
@@ -0,0 +1,29 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    
+  
+    if ($username === "admin" && $password === "admin") {
+
+
+
+        header("Location: http://localhost/my-e-diary/home.php");
+
+        exit();
+
+    } 
+    
+    else {
+        echo "
+        <script>
+           
+        alert('Incorrect username or password. Login Again.');
+           
+            window.location.replace('http://localhost/my-e-diary/');
+        </script>
+        ";
+    }
+}
+?>
